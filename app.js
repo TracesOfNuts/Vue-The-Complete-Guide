b0d873d4fc8c5da2e5921889768a1f32adba0ae2
tracesofnuts <ahmadzahiri90@gmail.com>
2022-05-31 18:07:30 +0800

    - Example of interpolation and data binding

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({ // have to pass an object as arg to createApp
     // },
     data() {
         return {
-            courseGoal: 'Finish the course!', // the return object can now be used in the Vue-controlled HTML
+            courseGoal: 'Finish the course!', // the return object can now be used in the Vue-controlled HTML by using a specific syntax called interpolation
         };
     }
 });
