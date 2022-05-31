b5ff0b800e80fd4855b68aa8da5d677c59c1caae
tracesofnuts <ahmadzahiri90@gmail.com>
2022-05-31 16:23:35 +0800

    refer to previous commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const app = Vue.createApp();
+const app = Vue.createApp({ // have to pass an object as arg to createApp
+    // data: function(){ // have to be called 'data' for Vue to recognize; can be re-written as just 'data(){...}'
+    //     return {}; // must return an object
+    // },
+    data() {
+        return {
+            courseGoal: 'Finish the course!', // the return object can now be used in the Vue-controlled HTML
+        };
+    }
+});
 
-app.mount('#user-goal');
+app.mount('#user-goal'); // mount() determines which part of the webpage to be controlled by Vue; it takes in a CSS selector string as input
